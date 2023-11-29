796b4896de1e8f16195bd05b6fe01d8e434a3b48
GTYaseen <Xxyaseent11xX14@gmail.com>
2023-11-29 03:37:05 +0300

    still metadata img

--- aon-blog/src/app/article/[id]/page.js
+++ aon-blog/src/app/article/[id]/page.js
@@ -1,42 +1,71 @@
-  import Header from "@/app/components/Header/Header";
-  import Container from "@/app/components/containter/Container";
-  import Footer from "@/app/components/footer/Footer";
-  import Styles from "./page.module.css";
-  import dayjs from "dayjs";
+import Header from "@/app/components/Header/Header";
+import Container from "@/app/components/containter/Container";
+import Footer from "@/app/components/footer/Footer";
+import Styles from "./page.module.css";
+import dayjs from "dayjs";
 
-  async function getData(id) {
-    const res = await fetch(
-      `https://api.slingacademy.com/v1/sample-data/blog-posts/${id}`
-    );
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    return res.json();
+// Define the getData function outside generateMetadata
+async function getData(id) {
+  const product = await fetch(
+    `https://api.slingacademy.com/v1/sample-data/blog-posts/${id}`
+  );
+  if (!product.ok) {
+    throw new Error("Failed to fetch data");
   }
+  return product.json();
+}
 
-  export default async function Article({ params }) {
-    let data = await getData(params.id);
-    return (
-      <main>
-        <Header />
-        <Container>
-          <div className={Styles.top}>
-            <div className={Styles.title}>
-              <h1>{data.blog.title}</h1>
-              <p>{data.blog.category}</p>
-            </div>
-            <div className={Styles.date}>
-              <p>{dayjs(data.blog.created_at).format("YYYY, MMM DD")}</p>
-            </div>
+export async function generateMetadata({ params }) {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  const data = await getData(id);
+
+  const photoUrl = data.blog.photo_url;
+  console.log(photoUrl);
+  return {
+    title: data.blog.title,
+    description: data.blog.description,
+    image: photoUrl,
+  };
+}
+
+export default async function Article({ params }) {
+  // Fetch data for the article
+  let data = await getData(params.id);
+
+  return (
+    <main>
+      <Header />
+      <Container>
+        <div className={Styles.top}>
+          <div className={Styles.title}>
+            <h1>{data.blog.title}</h1>
+            <p>{data.blog.category}</p>
           </div>
-          <div className={Styles.img}>
-            <img src={data.blog.photo_url} alt={data.blog.title} />
+          <div className={Styles.date}>
+            <p>{dayjs(data.blog.created_at).format("YYYY, MMM DD")}</p>
           </div>
-          <div className={Styles.content} dangerouslySetInnerHTML={{__html: `${data.blog.content_text.slice(0, 600)}...`}}/>
-          <div className={Styles.hr}><hr/></div>
-          <div className={Styles.fcontent} dangerouslySetInnerHTML={{__html: data.blog.content_html}}/>
-        </Container>
-        <Footer />
-      </main>
-    );
-  }
+        </div>
+        <div className={Styles.img}>
+          <img src={data.blog.photo_url} alt={data.blog.title} />
+        </div>
+        <div
+          className={Styles.content}
+          dangerouslySetInnerHTML={{
+            __html: `${data.blog.content_text.slice(0, 600)}...`,
+          }}
+        />
+        <div className={Styles.hr}>
+          <hr />
+        </div>
+        <div
+          className={Styles.fcontent}
+          dangerouslySetInnerHTML={{ __html: data.blog.content_html }}
+        />
+      </Container>
+      <Footer />
+    </main>
+  );
+}
