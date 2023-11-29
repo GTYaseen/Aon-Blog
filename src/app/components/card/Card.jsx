bced0b929f1cce6eade8b0fc5e1fba0b4f612e71
GTYaseen <Xxyaseent11xX14@gmail.com>
2023-11-29 22:22:08 +0300

    still

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -3,25 +3,30 @@ import Link from "next/link";
 import Image from "next/image";
 import dayjs from "dayjs";
 import FavBtn from "../favBtn/FavBtn";
+import "intro.js/introjs.css";
+import introJs from "intro.js";
+import { useEffect } from "react";
 
 const Card = ({ blog }) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.img}>
-        <Image src={blog.photo_url} alt={blog.title} fill />
-      </div>
-      <div className={styles.title}>
-        <h1>{blog.title}</h1>
-      </div>
-      <div className={styles.subTitle}>
-        <p>{blog.category}</p>
-      </div>
-      <div className={styles.footer}>
-        <div className={styles.read}>
-          <Link href={`/article/${blog.id}`}>Read Article </Link>
-          <FavBtn blog={blog} />
+    <div className="card-div">
+      <div className={styles.card}>
+        <div className={styles.img}>
+          <Image src={blog.photo_url} alt={blog.title} fill />
         </div>
-        <p>{dayjs(blog.created_at).format("YYYY, MMM DD")}</p>
+        <div className={styles.title}>
+          <h1>{blog.title}</h1>
+        </div>
+        <div className={styles.subTitle}>
+          <p>{blog.category}</p>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.read}>
+            <Link href={`/article/${blog.id}`}>Read Article </Link>
+            <FavBtn blog={blog} />
+          </div>
+          <p>{dayjs(blog.created_at).format("YYYY, MMM DD")}</p>
+        </div>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Card from "./components/card/Card";
@@ -7,13 +8,18 @@ import Footer from "./components/footer/Footer";
 import styles from "./page.module.css";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useStore } from "./store";
+import "intro.js/introjs.css";
+import introJs from "intro.js";
+import { useRef } from "react";
 
 export default function Home() {
+  const homeIntroRef = useRef();
+  const cardIntroRef = useRef();
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [skip, setSkip] = useState(0);
   const { setFavorite } = useStore();
-  // const [favorite, setFavorite] = useState([]);
+
   const blog = () => {
     fetch(
       `https://api.slingacademy.com/v1/sample-data/blog-posts?limit=12&offset=${skip}`
@@ -38,40 +44,74 @@ export default function Home() {
     if (fav) setFavorite(JSON.parse(fav));
   }, []);
 
+  useEffect(() => {
+    const initializeIntro = () => {
+      const steps = [
+        {
+          intro: "Welcome to the Simple Blog!",
+        },
+        {
+          element: ".h1",
+          intro: "This is the title",
+        },
+        {
+          element: ".card-div",
+          intro: "Explore the blog posts here!",
+        },
+        {
+          element: ".header",
+          intro: "This is the header",
+        }
+      ];
+
+      homeIntroRef.current = introJs().setOptions({
+        steps,
+        showBullets: false,
+      });
+
+      // Start home intro after a delay
+      setTimeout(() => {
+        homeIntroRef.current.start();
+      }, 1000);
+    };
+
+    initializeIntro();
+  }, []);
+
   return (
     <main className={styles.home}>
-      <Header />
+      <div className="header">
+      <Header/>
+      </div>
       <div className={styles.cover}>
         <div className={styles.overlay}>
           <div className={styles.title}>
             <Container>
-              <h1>Simple Blog.</h1>
+              <h1 className="h1">Simple Blog.</h1>
               <p>A blog created by Aon 2023</p>
             </Container>
           </div>
         </div>
       </div>
-      <div className={styles.over}>
-        <Container>
-          <InfiniteScroll
-            dataLength={list.length}
-            next={() => setSkip(skip + 12)}
-            hasMore={true}
-            className={styles.over}
-            endMessage={
-              <p style={{ textAlign: "center" }}>you have seen it all</p>
-            }
-            loader={<span className={styles.loader}></span>}
-          >
-            <div className={styles.cardContainer}>
-              {loading && <span className={styles.loader}>Loading</span>}
-              {list.map((blog, index) => (
-                <Card blog={blog} key={index} />
-              ))}
-            </div>
-          </InfiniteScroll>
-        </Container>
-      </div>
+      <Container>
+        <div className="card-div">
+          <div className={styles.over}>
+            <InfiniteScroll
+              dataLength={list.length}
+              next={() => setSkip(skip + 12)}
+              hasMore={true}
+              className={styles.over}
+            >
+              <div className={styles.cardContainer}>
+                {loading && <span className={styles.loader}>Loading</span>}
+                {list.map((blog, index) => (
+                  <Card blog={blog} key={index} introRef={cardIntroRef} />
+                ))}
+              </div>
+            </InfiniteScroll>
+          </div>
+        </div>
+      </Container>
       <Footer />
     </main>
   );
