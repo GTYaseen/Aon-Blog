1689164b1134643999ecdd7c5d20729470c88a0a
GTYaseen <Xxyaseent11xX14@gmail.com>
2023-11-29 18:32:18 +0300

    Update page.js

--- src/app/article/[id]/page.js
+++ src/app/article/[id]/page.js
@@ -16,56 +16,54 @@ async function getData(id) {
 }
 
 export async function generateMetadata({ params }) {
-  // read route params
   const id = params.id;
 
-  // fetch data
   const data = await getData(id);
 
-  const photoUrl = data.blog.photo_url;
-  console.log(photoUrl);
+  console.log(data.blog.photo_url);
   return {
     title: data.blog.title,
     description: data.blog.description,
-    image: photoUrl,
+    image: data.blog.photo_url,
   };
 }
 
 export default async function Article({ params }) {
-  // Fetch data for the article
   let data = await getData(params.id);
 
   return (
-    <main>
-      <Header />
-      <Container>
-        <div className={Styles.top}>
-          <div className={Styles.title}>
-            <h1>{data.blog.title}</h1>
-            <p>{data.blog.category}</p>
+    <>
+      <main>
+        <Header />
+        <Container>
+          <div className={Styles.top}>
+            <div className={Styles.title}>
+              <h1>{data.blog.title}</h1>
+              <p>{data.blog.category}</p>
+            </div>
+            <div className={Styles.date}>
+              <p>{dayjs(data.blog.created_at).format("YYYY, MMM DD")}</p>
+            </div>
           </div>
-          <div className={Styles.date}>
-            <p>{dayjs(data.blog.created_at).format("YYYY, MMM DD")}</p>
+          <div className={Styles.img}>
+            <img src={data.blog.photo_url} alt={data.blog.title} />
           </div>
-        </div>
-        <div className={Styles.img}>
-          <img src={data.blog.photo_url} alt={data.blog.title} />
-        </div>
-        <div
-          className={Styles.content}
-          dangerouslySetInnerHTML={{
-            __html: `${data.blog.content_text.slice(0, 600)}...`,
-          }}
-        />
-        <div className={Styles.hr}>
-          <hr />
-        </div>
-        <div
-          className={Styles.fcontent}
-          dangerouslySetInnerHTML={{ __html: data.blog.content_html }}
-        />
-      </Container>
-      <Footer />
-    </main>
+          <div
+            className={Styles.content}
+            dangerouslySetInnerHTML={{
+              __html: `${data.blog.content_text.slice(0, 600)}...`,
+            }}
+          />
+          <div className={Styles.hr}>
+            <hr />
+          </div>
+          <div
+            className={Styles.fcontent}
+            dangerouslySetInnerHTML={{ __html: data.blog.content_html }}
+          />
+        </Container>
+        <Footer />
+      </main>
+    </>
   );
 }
